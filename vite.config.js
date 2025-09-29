52135ad6497c2870613236fc2d9911ec784d586f
Isabella Hermano <isabellahermano618@gmail.com>
2025-09-29 00:46:26 -0500

    Fix deployment config again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change this from '/' to './'
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: 'https://isahermano.github.io/',
 })
